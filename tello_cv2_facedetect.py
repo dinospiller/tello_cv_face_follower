91ad2f95f735871a637bd8e0506f84a76af42e1d
Dino Spiller <dinospiller@gmail.com>
2019-02-06 23:04:55 +0100

    added buttons functions

--- tello_cv2_facedetect.py
+++ tello_cv2_facedetect.py
@@ -14,16 +14,19 @@ from facedetector import FaceDetector
 
 fd = FaceDetector("haarcascade_frontalface_default.xml")
 
+font=cv2.FONT_HERSHEY_SIMPLEX
+
 def main():
     drone = tellopy.Tello()
+    detection=0; # face detectio active flag
 
     try:
         drone.connect()
         drone.wait_for_connection(60.0)
 
         container = av.open(drone.get_video_stream())
-        # skip first 300 frames
-        frame_skip = 300
+        # skip first frames
+        frame_skip = 30
         while True:
             for frame in container.decode(video=0):
                 if 0 < frame_skip:
@@ -32,20 +35,41 @@ def main():
                 start_time = time.time()
                 image = cv2.cvtColor(np.array(frame.to_image()), cv2.COLOR_RGB2BGR)
                 image=imutils.resize(image,width=700)
-                image_bw=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+                
+                if detection == 0:
+                    cv2.putText(image,"press d to start face detection",(20,20),font,0.5,(0,255,0),2,cv2.LINE_AA);
+                else:
+                    image_bw=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 
-                (faceRects,face_areas) = fd.track_faces(image_bw)
+                    (faceRects,face_areas) = fd.track_faces(image_bw)
 
-                for face_area in face_areas:
-                    cv2.rectangle(image, (face_area[0], face_area[1]), (face_area[2], face_area[3]), (0, 255, 0), 2)
+                    ind = 1;
+                    for face_area in face_areas:
+                        if(ind==1): #distinguish between first and successive faces (follow only the first)
+                            color=(0,255,0)
+                        else:
+                            color=(255,0,0)
+                        cv2.rectangle(image, (face_area[0], face_area[1]), (face_area[2], face_area[3]), color, 2)
+                        ind=ind+1;
+
+                    cv2.putText(image,"press d to STOP detection",(20,20),font,0.5,(0,255,0),2,cv2.LINE_AA);
+
     
+                cv2.putText(image,"press q to exit",(20,40),font,0.5,(0,255,0),2,cv2.LINE_AA);
+
+                cv2.imshow("Drone Camera", image)
     
-                cv2.imshow("Faces detected", image)
-    
-                print("I found: "+ str(len(faceRects)) +" face(s)")
-                #cv2.waitKey(1)
-                if cv2.waitKey(1) & 0xFF == ord("q"):
-                   break
+                #print("I found: "+ str(len(faceRects)) +" face(s)")
+                but_pressed=cv2.waitKey(1)
+                if but_pressed & 0xFF == ord("q"):
+                    cv2.destroyAllWindows()
+                    drone.quit()
+                    break
+                elif but_pressed & 0xFF == ord("d"):
+                    if detection==1:
+                        detection=0
+                    else:
+                        detection=1
 
                 if frame.time_base < 1.0/60:
                     time_base = 1.0/60
